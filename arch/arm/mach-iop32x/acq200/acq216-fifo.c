3d630e902989bc289ba4cd39b588349eaf704014
Peter Milne <Peter.Milne@d-tacq.com>
2007-04-26 07:23:11 +0100

    uses iop32x_check_pci_bus_speed() to select OB_CLOCK source NB: at 100MHz, ob_clock source is 25MHz, not 16MHz This should give better jitter performance? 	modified:   arch/arm/mach-iop32x/acq200/acq216-fifo.c

--- arch/arm/mach-iop32x/acq200/acq216-fifo.c
+++ arch/arm/mach-iop32x/acq200/acq216-fifo.c
@@ -148,6 +148,7 @@ static unsigned check_fifstat(
 
 #define CHECK_FIFSTAT(wo, fifstat, offset) check_fifstat(wo, fifstat, offset)
 
+extern int iop32x_pci_bus_speed(void);
 
 static int set_ob_clock(int hz)
 /* set ICS307 clock */
@@ -158,20 +159,26 @@ static int set_ob_clock(int hz)
 		0
 	};
 
-	static char arg1[20];
-	static char *argv[4];
+	static char fout_def[20];
+	static char fin_def[20];
+	static char *argv[6];
 	int i;
+	int fin;
+
+	sprintf(fin_def, "%d", iop32x_pci_bus_speed());
 
 /** source OB clock from INT 16M, source int clock divider from OBC, 
  *  see store_ob_clock_word() 
  */
 	CAPDEF->ob_clk_src->DIx = ACQ200_CLKCON_DLL_CS_16M;
 
-	sprintf(arg1, "%d", hz/1000);
+	sprintf(fout_def, "%d", hz/1000);
 
         i = 0;
         argv[i++] = "/usr/local/bin/ob_calc";
-        argv[i++] = arg1;
+	argv[i++] = "--fin";
+	argv[i++] = fin_def;	
+        argv[i++] = fout_def;
 	argv[i++] = "/dev/dtacq/ob_clock_word";
         argv[i] = 0;
 
@@ -1218,7 +1225,7 @@ static ssize_t acq200_fpga_fifo_read_buf_read (
 
 	dbg( 2, "len %d *offset %d", len, (int)*offset );
 
-	copy_to_user( buf, va_buf_offset( DG, *offset ), len );
+	COPY_TO_USER( buf, va_buf_offset( DG, *offset ), len );
 	*offset += len;
 
 	return len;
@@ -1267,7 +1274,7 @@ acq200_fpga_live_read_one_frame_per_dcb(
 	dma_sync_single(DG->dev, dcb->handle+dcb->last_start, 
 			len, DMA_FROM_DEVICE);
 
-        copy_to_user(buf, base+dcb->last_start, len);
+        COPY_TO_USER(buf, base+dcb->last_start, len);
 	*offset += len;
 	
 	return len;
