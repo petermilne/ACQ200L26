9d6da171ca2232ad8b53540be94178f5901f5111
Peter Milne <Peter.Milne@d-tacq.com>
2009-08-30 18:33:17 +0100

    acq164 compiles with acq164 FPGA defs. This will still need a _lot_ of work Tentative split of commons into acq100.h NB: still unsure as to what parts of FPGA (if any) to treat as common In general - addresses - all explicit fields - extract common field patterns In general, err on the side of duplication 	new file:   arch/arm/mach-iop32x/acq200/acq100-fpga.c 	new file:   arch/arm/mach-iop32x/acq200/acq100.h 	new file:   arch/arm/mach-iop32x/acq200/acq164.h 	modified:   .gitignore 	modified:   arch/arm/mach-iop32x/acq200/acq100-llc.c 	modified:   arch/arm/mach-iop32x/acq200/acq132-fifo.c 	modified:   arch/arm/mach-iop32x/acq200/acq132-fiq.S 	modified:   arch/arm/mach-iop32x/acq200/acq132-offset.c 	modified:   arch/arm/mach-iop32x/acq200/acq132.h 	modified:   arch/arm/mach-iop32x/acq200/acq164-fifo.c 	modified:   arch/arm/mach-iop32x/acq200/acq196-fifo.c 	modified:   arch/arm/mach-iop32x/acq200/acq196.h

---
+++ arch/arm/mach-iop32x/acq200/acq100.h
@@ -0,0 +1,266 @@
+/* ------------------------------------------------------------------------- */
+/* acq100.h - common FPGA reg defs for ACQ100 series                         */
+/* ------------------------------------------------------------------------- */
+/*   Copyright (C) 2009 Peter Milne, D-TACQ Solutions Ltd
+ *                      <Peter dot Milne at D hyphen TACQ dot com>
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of Version 2 of the GNU General Public License
+    as published by the Free Software Foundation;
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
+/* ------------------------------------------------------------------------- */
+
+
+#ifndef __ACQ100_H__
+#define __ACQ100_H__
+
+
+#ifdef __ASSEMBLER__
+#define FPGA_REG(offset)   offset
+#else
+#define FPGA_REG(offset) ((volatile u32*)((unsigned)ACQ200_FPGA+offset))
+#endif
+
+#define ACQ100_SYSCON		FPGA_REG(0x0c)
+#define ACQ100_SYSCON_DAC	FPGA_REG(0x18)
+#define ACQ200_DIOCON		FPGA_REG(0x24)
+#define ACQ100_OFFSET_DACS	FPGA_REG(0x28)
+#define ACQ100_WAVLIMIT		FPGA_REG(0x2c)
+
+#define ACQ200_ICR_OFFSET 0x80
+#define ACQ200_ICR     FPGA_REG( ACQ200_ICR_OFFSET )
+
+#define ACQ100_BDR_DEFAULT 0xdeadbeef
+
+#define ACQ100_SYSCON_EV_MASK    0xf
+#define ACQ100_SYSCON_EV_DISABLE 0x0
+#define ACQ100_SYSCON_EV_RISING  0x8
+#define ACQ100_SYSCON_EV_FALLING 0x0
+#define ACQ100_SYSCON_EV_DI0     0x1
+#define ACQ100_SYSCON_EV_DI1     0x2
+#define ACQ100_SYSCON_EV_DI2     0x3
+#define ACQ100_SYSCON_EV_DI3     0x4
+#define ACQ100_SYSCON_EV_DI4     0x5
+#define ACQ100_SYSCON_EV_DI5     0x6
+#define ACQ100_SYSCON_EV_RES1    0x7
+
+#define ACQ100_SYSCON_EV0_SHIFT 8
+#define ACQ100_SYSCON_EV1_SHIFT 12
+#define ACQ100_SYSCON_TRG_SHIFT 16
+
+
+#define ACQ100_ICS527_FDW	0x007f0000
+#define ACQ100_ICS527_RDW	0x00007f00
+#define ACQ100_ICS527_S1S0	0x000000c0
+
+#define ACQ200_DIOCON_SETOUT    0x00ff0000
+#define ACQ200_DIOCON_OUTDAT    0x0000ff00
+#define ACQ200_DIOCON_INPDAT    0x000000ff
+
+#define ACQ200_DIOCON_SETOUT_SHL 16
+#define ACQ200_DIOCON_OUTDAT_SHL 8
+#define ACQ200_DIOCON_INPDAT_SHL 0
+
+
+#define ACQ100_OFFSET_DACS_CHIPSEL 0x80000000
+#define ACQ100_OFFSET_DACS_HSHAKE  0x40000000
+
+#define ACQ100_OFFSET_DACS_ADDRX   0xf
+
+
+#define ACQ100_OFFSET_DACS_ADDR0   0x0
+
+#define DACA 0x1
+#define DACB 0x2
+#define DACC 0x3
+#define DACD 0x4
+#define DACE 0x5
+#define DACF 0x6
+#define DACG 0x7
+#define DACH 0x8
+
+#define ACQ100_OFFSET_DACS_XSHIFT  16
+#define ACQ100_OFFSET_DACS_YSHIFT   0
+#define ACQ100_OFFSET_DACS_ASHIFT  10
+
+
+#define ACQ100_WAVLIMIT_FAWG_DIV   0x00ff0000U
+#define ACQ100_WAVLIMIT_WAVLIMIT   0x000001ffU
+
+#define ACQ100_WAVLIMIT_FAWG_DIV_SHL 16
+
+
+#ifdef PGMCOMOUT
+/*
+ * cold FIFO 8K. 
+ * in V1, 0x1F = 8K, 0x10=4K 0x8=2K
+ */
+/*
+ * REGISTERS
+ */
+
+#define ACQ100_BDR      FPGA_REG(0x00)
+#define ACQ100_FIFCON   FPGA_REG(0x04)
+#define ACQ100_FIFSTAT  FPGA_REG(0x08)
+#define ACQ100_SYSCON   FPGA_REG(0x0c)
+
+#define ACQ200_DIOCON   FPGA_REG( 0x24 )
+#define ACQ100_OFFSET_DACS FPGA_REG(0x28)
+#define ACQ100_WAVLIMIT FPGA_REG(0x2c)
+
+#define ACQ100_TCR_IMMEDIATE FPGA_REG(0x44)
+#define ACQ100_TCR_LATCH     FPGA_REG(0x48)
+
+#define ACQ100_SYSCON_CLKVAL    0x00000008
+#define ACQ100_SYSCON_TRIGGERED 0x00000004
+#define ACQ100_SYSCON_SOFTTRIG  0x00000002
+#define ACQ100_SYSCON_ACQEN     0x00000001
+
+#define ACQ100_FIFSTAT_HOTPOINT   0x0000000f
+
+
+
+#define ACQ200_DIOCON_SETOUT    0x00ff0000
+#define ACQ200_DIOCON_OUTDAT    0x0000ff00
+#define ACQ200_DIOCON_INPDAT    0x000000ff
+
+#define ACQ200_DIOCON_SETOUT_SHL 16
+#define ACQ200_DIOCON_OUTDAT_SHL 8
+#define ACQ200_DIOCON_INPDAT_SHL 0
+
+
+/** Counter compatibility with ACQ216: @@worktodo */
+
+#define ACQ216_TCR_IMM  ACQ100_TCR_IMMEDIATE
+#define ACQ216_TCR_LAT  ACQ100_TCR_LATCH
+
+#define ACQ216_TCR_RUNNING  0x80000000
+#define ACQ216_TCR_UPDCTRL  0x18000000
+#define ACQ216_TCR_TCS_MASK 0x07000000
+#define ACQ216_TCR_COUNTER  0x00000fff
+
+#define ACQ216_TCR_TCS_SHL 24
+#define ACQ216_TCR_UPDCTRL_SHL 27
+
+#ifndef __ASSEMBLER__
+
+extern int disable_acq_debug;
+#define DISABLE_ACQ_DEBUG (!disable_acq_debug)
+
+extern void disable_acq(void);
+extern void enable_acq(void);
+
+
+static inline u32 acq100_syscon_set(u32 flags) {
+	return *ACQ100_SYSCON |= flags;
+}
+
+static inline u32 acq100_syscon_clr(u32 flags){
+	return *ACQ100_SYSCON &= ~flags;
+}
+
+static inline u32 acq100_fifcon_set(u32 flags){
+	return *ACQ100_FIFCON |= flags;
+}
+
+static inline u32 acq100_fifcon_clr(u32 flags){
+	return *ACQ100_FIFCON &= ~flags;
+}
+static inline u32 acq100_getSyscon(void) {
+	return *ACQ100_SYSCON;
+}
+
+#endif
+
+#endif	/* PGMCOMOUT */
+
+#ifndef __ASSEMBLER__
+
+
+static inline void setFAWG_DIV(unsigned div) {
+	u32 limit = *ACQ100_WAVLIMIT;
+
+	div = max(div, 1U);
+	div = min(div, 256U);
+	
+	/** factor 2 adjust reflects realite' */
+	div *= 2;
+	
+	limit &= ~(0xff << ACQ100_WAVLIMIT_FAWG_DIV_SHL);
+	limit |= (div-1) << ACQ100_WAVLIMIT_FAWG_DIV_SHL;
+
+	*ACQ100_WAVLIMIT = limit;
+}
+static inline unsigned getFAWG_DIV(void) {
+	unsigned div = (((*ACQ100_WAVLIMIT & ACQ100_WAVLIMIT_FAWG_DIV) >>
+		ACQ100_WAVLIMIT_FAWG_DIV_SHL) + 1);
+	/** factor 2 adjust reflects realite' */	
+	return div / 2;
+}
+
+static inline void setWAVLIMIT(unsigned limit) {	
+	u32 acq196_wavlimit = *ACQ100_WAVLIMIT;
+
+	limit -= 1;
+	limit = min(limit, ACQ100_WAVLIMIT_WAVLIMIT);
+
+	acq196_wavlimit &= ~ACQ100_WAVLIMIT_WAVLIMIT;
+	acq196_wavlimit |= limit;
+
+	*ACQ100_WAVLIMIT = acq196_wavlimit;	
+}
+
+static inline unsigned getWAVLIMIT(void)
+{
+	return *ACQ100_WAVLIMIT&ACQ100_WAVLIMIT_WAVLIMIT;
+}
+
+static inline u32 acq100_lineCode(int DIx)
+{
+	return DIx + 1; 
+}
+
+extern int disable_acq_debug;
+#define DISABLE_ACQ_DEBUG (!disable_acq_debug)
+
+static inline u32 reg_set_field(volatile u32* reg, int shift, u32 mask, u32 value)
+{
+	u32 rv = *reg;
+	rv &= ~ (mask << shift);
+	rv |= (value << shift);
+	*reg = rv;
+	return rv;
+}
+
+#endif	/* __ASSEMBLER__ */
+
+
+
+#define ACQ216_TCR_RUNNING  0x80000000
+#define ACQ216_TCR_UPDCTRL  0x18000000
+#define ACQ216_TCR_TCS_MASK 0x07000000
+#define ACQ216_TCR_COUNTER  0x00000fff
+
+#define ACQ216_TCR_TCS_SHL 24
+#define ACQ216_TCR_UPDCTRL_SHL 27
+
+
+#ifdef HAS_OB_CLOCK
+struct OB_CLOCK_DEF {
+	int demand;
+	int actual;
+	int FDW;
+	int RDW;
+	int R;
+	int Sx;
+};
+
+extern struct OB_CLOCK_DEF ob_clock_def;
+#endif
+
+#endif	/*  __ACQ100_H__ */
---
+++ arch/arm/mach-iop32x/acq200/acq164.h
@@ -0,0 +1,227 @@
+/* ------------------------------------------------------------------------- */
+/* acq164.h - acq164 FPGA regs                                               */
+/* ------------------------------------------------------------------------- */
+/*   Copyright (C) 2009 Peter Milne, D-TACQ Solutions Ltd
+ *                      <Peter dot Milne at D hyphen TACQ dot com>
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of Version 2 of the GNU General Public License
+    as published by the Free Software Foundation;
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
+/* ------------------------------------------------------------------------- */
+
+
+#ifndef __ACQ132_H__
+#define __ACQ132_H__
+
+#define HAS_OB_CLOCK	1
+#include "acq100.h"
+
+#define ACQ164_BDR		FPGA_REG(0x00)
+#define ACQ164_FIFCON		FPGA_REG(0x04)
+#define ACQ164_FIFSTAT		FPGA_REG(0x08)
+#define ACQ164_SYSCON		FPGA_REG(0x0c)
+#define ACQ164_ICS527		FPGA_REG(0x14)
+#define ACQ164_SYSCONDAC	FPGA_REG(0x18)
+#define ACQ164_CLKCON		FPGA_REG(0x1c)
+#define ACQ164_DIOCON		ACQ200_DIOCON		/* FPGA_REG(0x24) */
+#define ACQ164_OFFSET_DACS	ACQ100_OFFSET_DACS	/* FPGA_REG(0x28) */
+#define ACQ164_WAVLIMIT		ACQ100_WAVLIMIT		/* FPGA_REG(0x2c) */
+#define ACQ164_TCR_IMMEDIATE	FPGA_REG(0x44)		/* same as 196? */
+#define ACQ164_TCR_LATCH	FPGA_REG(0x48)		/* same as 196? */
+#define ACQ164_RGATE		FPGA_REG(0x50)		/* same as 196? */
+#define ACQ164_CLK_COUNTER	FPGA_REG(0x54)	/* same as 132 */
+
+#define ACQ164_FIFO_OFFSET     0x00100000
+
+
+#define ACQ164_FIFCON_DAC_IE	  0x80000000
+/** not used                      0x40000000 */
+#define ACQ164_FIFCON_DAC_RESET   0x20000000
+#define ACQ164_FIFCON_DAC_ENABLE  0x10000000
+#define ACQ164_FIFCON_DAC_LTIDE   0x0f000000
+/** not used			  0x00fe0000 */
+#define ACQ164_FIFCON_THROW_EN	  0x00010000
+/** not used			  0x0000f000 */
+
+#define ACQ164_FIFCON_ADC2_RESET  0x00000800
+#define ACQ164_FIFCON_ADC2_ENABLE 0x00000400
+#define ACQ164_FIFCON_ADC1_RESET  0x00000200
+#define ACQ164_FIFCON_ADC1_ENABLE 0x00000100
+#define ACQ164_FIFCON_HOT_IE      0x00000080
+#define ACQ164_FIFCON_HOT_RESET   0x00000040
+#define ACQ164_FIFCON_HOT_ENABLE  0x00000020
+#define ACQ164_FIFCON_HOT_HITIDE  0x0000001f
+
+
+#define ACQ164_FIFCON_ADCX_ENABLE \
+	(ACQ164_FIFCON_ADC2_ENABLE|ACQ164_FIFCON_ADC1_ENABLE)
+#define ACQ164_FIFCON_ADCX_RESET \
+	(ACQ164_FIFCON_ADC2_RESET|ACQ164_FIFCON_ADC1_RESET)
+
+
+
+#define ACQ164_FIFSTAT_DAC_TR     0x80000000
+#define ACQ164_FIFSTAT_DAC_LT     0x40000000
+#define ACQ164_FIFSTAT_FAWG_PTR   0x3f000000
+#define ACQ164_FIFSTAT_ADC_EV1    0x00800000  /* RC */
+#define ACQ164_FIFSTAT_ADC_EV0    0x00400000  /* RC */
+#define ACQ164_FIFSTAT_ADC_TR     0x00200000  /* RO */
+/** not used			  0x001f0000 */
+/* reserved                       0x00008000 */
+#define ACQ164_FIFSTAT_ADC2_NE    0x00004000
+#define ACQ164_FIFSTAT_ADC2_OVER  0x00002000
+#define ACQ164_FIFSTAT_ADC2_UNDER 0x00001000
+/* reserved                       0x00000800 */
+#define ACQ164_FIFSTAT_ADC1_NE    0x00000400
+#define ACQ164_FIFSTAT_ADC1_OVER  0x00000200
+#define ACQ164_FIFSTAT_ADC1_UNDER 0x00000100
+
+#define ACQ164_FIFSTAT_HOT_HT     0x00000080
+#define ACQ164_FIFSTAT_HOT_OVER   0x00000040
+#define ACQ164_FIFSTAT_HOT_UNDER  0x00000020
+#define ACQ164_FIFSTAT_HOT_NE     0x00000010
+#define ACQ164_FIFSTAT_HOTPOINT   0x0000000f
+
+#define ACQ164_FIFSTAT_ADC_EVX	\
+	(ACQ164_FIFSTAT_ADC_EV1|ACQ164_FIFSTAT_ADC_EV0)
+
+#define ACQ164_SYSCON_REV_RESET 0x80000000
+/** not used			0x78000000 */
+#define ACQ164_SYSCON_OTR_MASK  0x07000000
+/** not used			0x00c00000 */
+#define ACQ164_SYSCON_CODE_OB   0x00200000
+#define ACQ164_SYSCON_LOWLAT	0x00100000
+#define ACQ164_SYSCON_TR_RISING 0x00080000
+#define ACQ164_SYSCON_TR_DIO    0x00070000
+#define ACQ164_SYSCON_EV1_EDGE  0x00008000
+#define ACQ164_SYSCON_EV1_DIO   0x00007000
+#define ACQ164_SYSCON_EV0_EDGE  0x00000800
+#define ACQ164_SYSCON_EV0_DIO   0x00000700
+#define ACQ164_SYSCON_LLSYNC	0x00000080
+#define ACQ164_SYSCON_CDM	0x00000070
+#define ACQ164_SYSCON_CLKDIV	0x00000040
+#define ACQ164_SYSCON_ADCMODE	0x00000030
+#define ACQ164_SYSCON_CLKVAL    0x00000008
+#define ACQ164_SYSCON_TRIGGERED 0x00000004
+#define ACQ164_SYSCON_SOFTTRIG  0x00000002
+#define ACQ164_SYSCON_ACQEN     0x00000001
+
+
+#define ACQ164_SYSCON_EV0_SHIFT 8
+#define ACQ164_SYSCON_EV1_SHIFT 12
+#define ACQ164_SYSCON_TRG_SHIFT 16
+
+#define ACQ196_SYSCON_EC_SHIFT  5
+
+
+#define ACQ164_SYSCON_OTR_SHIFT	24
+
+#define ACQ164_SYSCON_OTR_DIS	0x0
+#define ACQ164_SYSCON_OTR_DO3	0x1
+#define ACQ164_SYSCON_OTR_DO4	0x2
+#define ACQ164_SYSCON_OTR_DO5	0x3
+
+#define ACQ164_SYSCON_CDM_SHIFT	4
+#define ACQ164_SYSCON_CDM_HISPEED_256 0x0
+#define ACQ164_SYSCON_CDM_HIRES_512   0x1
+#define ACQ164_SYSCON_CDM_LP_256      0x2
+#define ACQ164_SYSCON_CDM_LP_512      0x6
+#define ACQ164_SYSCON_CDM_LS_512      0x3
+
+/* SYSCONDAC : want exact same as ACQ196 please */
+
+/** not used				0xffff0000 */
+#define ACQ164_CLKCON_OIND_RISING	0x00008000
+/** not used				0x00004000 */
+#define ACQ164_CLKCON_OIND_MASK		0x00003000
+#define ACQ164_CLKCON_IND_RISING	0x00000800
+/* not used				0x00000400 */
+#define ACQ164_CLKCON_IND_MASK		0x00000300
+#define ACQ164_CLKCON_OCS_RISING	0x00000080
+/* not used				0x00000040 */
+#define ACQ164_CLKCON_OCS_MASK		0x00000030
+#define ACQ164_CLKCON_CS_RISING		0x00000008
+/* not used				0x00000004 */
+#define ACQ164_CLKCON_CS_MASK		0x00000003
+
+
+#define ACQ164_CLKCON_OIND_SHIFT	12
+#define ACQ164_CLKCON_IND_SHIFT		8
+#define ACQ164_CLKCON_OCS_SHIFT		4
+#define ACQ164_CLKCON_CS_SHIFT		0
+
+#define ACQ164_CLKCON_DIS		0x0
+#define ACQ164_CLKCON_D0		0x1
+#define ACQ164_CLKCON_D1		0x2
+#define ACQ164_CLKCON_D2		0x3
+
+#define ACQ164_CLKCON_XX		0x3
+#define ACQ164_CLKCON_32768		0x0
+
+#define ACQ100_ICR_HOTEN 0x00000080
+#define ACQ100_ICR_DACEN 0x80000000
+
+
+
+
+static inline u32 acq164_syscon_set(u32 flags) {
+	return *ACQ164_SYSCON |= flags;
+}
+
+static inline u32 acq164_syscon_clr(u32 flags){
+	return *ACQ164_SYSCON &= ~flags;
+}
+
+static inline u32 acq164_fifcon_set(u32 flags){
+	return *ACQ164_FIFCON |= flags;
+}
+
+static inline u32 acq164_fifcon_clr(u32 flags){
+	return *ACQ164_FIFCON &= ~flags;
+}
+static inline u32 acq164_getSyscon(void) {
+	return *ACQ164_SYSCON;
+}
+
+static inline u32 acq164_syscon_dac_set(u32 flags) {
+	return *ACQ100_SYSCON_DAC |= flags;
+}
+static inline u32 acq164_syscon_dac_clr(u32 flags){
+	return *ACQ100_SYSCON_DAC &= ~flags;
+}
+
+static inline void enable_fifo(void)
+{
+	acq164_fifcon_set(ACQ164_FIFCON_ADCX_ENABLE);	
+}
+static inline void disable_fifo(void)
+{
+	dbg(DISABLE_ACQ_DEBUG, "");
+	acq164_fifcon_clr(ACQ164_FIFCON_ADCX_ENABLE);
+}
+
+
+
+
+static inline void stop_capture(void)
+{
+	dbg(DISABLE_ACQ_DEBUG, "");
+	disable_acq();
+	disable_fifo();
+}
+
+
+/** Counter compatibility with ACQ216: @@worktodo */
+
+#define ACQ216_TCR_IMM  ACQ164_TCR_IMMEDIATE
+#define ACQ216_TCR_LAT  ACQ164_TCR_LATCH
+
+extern struct OB_CLOCK_DEF ob_clock_def;
+
+#endif	/*  __ACQ132_H__ */
