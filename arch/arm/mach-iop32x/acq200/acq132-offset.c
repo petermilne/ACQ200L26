8c08e0af183d5d77c59a74968bab27bc674834c3
Peter Milne <peter.milne@d-tacq.com>
2011-03-23 10:38:16 +0000

    there's no offset circuit in ACQ132 any more. Remove it 	modified:   arch/arm/mach-iop32x/acq200/acq132-offset.c

--- arch/arm/mach-iop32x/acq200/acq132-offset.c
+++ arch/arm/mach-iop32x/acq200/acq132-offset.c
@@ -21,29 +21,6 @@
 #define ACQ132
 #include "acq100-offset-inc.h"
 
-/*
- * map chip, dac to channel in block
- */
-static const int MAP[NCHIPSBLOCK+1][NDACSCHIP+1] = {
-/* MAP[chip][dac] ... unfortunately, these numbers are pchan order :-( */
-	[ 1][DACA] = 17,	[ 2][DACA] = 25,  
-	[ 1][DACB] = 18,	[ 2][DACB] = 26,
-	[ 1][DACC] = 19,	[ 2][DACC] = 27,
-	[ 1][DACD] = 20,	[ 2][DACD] = 28,
-	[ 1][DACE] = 21,	[ 2][DACE] = 29, 
-	[ 1][DACF] = 22,	[ 2][DACF] = 30,
-	[ 1][DACG] = 23,	[ 2][DACG] = 31,
-	[ 1][DACH] = 24,	[ 2][DACH] = 32,
-	
-	[ 3][DACA] =  1,	[ 4][DACA] =  9,   
-	[ 3][DACB] =  2,	[ 4][DACB] = 10,
-	[ 3][DACC] =  3,	[ 4][DACC] = 11,
-	[ 3][DACD] =  4,	[ 4][DACD] = 12,
-	[ 3][DACE] =  5,	[ 4][DACE] = 13,
-	[ 3][DACF] =  6,	[ 4][DACF] = 14,
-	[ 3][DACG] =  7,	[ 4][DACG] = 15,
-	[ 3][DACH] =  8,	[ 4][DACH] = 16,	 
-};
 
 
 const int acq132_default_plut[] = {
@@ -76,14 +53,6 @@ static const int *plut = acq132_default_plut;
 static int nlut = __PLUT_ELEMS;
 
 
-static int hw_fudge(int lchan)
-{
-	switch(lchan){
-	default:
-		return lchan;
-	}
-}
-
 
 int acq200_lookup_pchan(int lchannel)
 /** lchannel in = 1:32 nameplate order return 0:32 memory order */
@@ -104,16 +73,6 @@ int acq200_lookup_lchan(int pchan)
 	return plut[pchan+1];
 }
 
-static inline unsigned short *key2offset(int ikey)
-/* ikey 1..32. in this case, NO pchan lookup is required */
-{
-	int block = (ikey-1)/NCHANNELSBLOCK + 1;
-	int lchan = (ikey-1)%NCHANNELSBLOCK + 1;
-
-	return &offsets[block][lchan];
-}
-
-#include "acq100-offset-inc.c"
 
 EXPORT_SYMBOL_GPL(acq200_lookup_lchan);
 EXPORT_SYMBOL_GPL(acq132_default_plut);
